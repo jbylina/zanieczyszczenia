4c2078f872939289f9883e92e4e0d65aeb708bc3
Banul <banul2@wp.pl>
2016-11-30 16:49:17 +0100

    interpolacja wielomianami 10 stopnia, wszystkie wykresy rysują się, sparsować datę, zoptymalizować kod bo słabo napisany, dodać jakieś bajery, poprawić szatę graficzną, dokonać predykcji poziomu zanieczyszczeń, zrobić ściąganie danych bezpośrednio z bazy danych (obecnie ściąganie z pliku csv, który jest ręczną kopią bazy danych)

--- server.R
+++ server.R
@@ -9,42 +9,75 @@ shinyServer(function(input, output) {
   output$wykres <- renderPlot({
       
     x=seq(input$Daty[1],input$Daty[2], by = 1)
-    
+    xdwa=x^2
+    xtrzy=x^3
+    xcztery=x^4
+    xpiec=x^5
+    xszesc=x^6
+    xsiedem=x^7
+    xosiem = x^8
+    xdziewiec = x^9
+    xdziesiec = x^10
+     
    
      if(input$opcje == 'O3')
      {
         y=c(O3[x])
         plot(x,y) 
-     }
+        fit=lm(y~x+xdwa+xtrzy+xcztery+xpiec+xszesc+xsiedem+xosiem+xdziewiec+xdziesiec)
+        xv=seq(min(x),max(x),0.01)
+        yv = predict(fit, list (x=xv, xdwa=xv^2, xtrzy=xv^3, xcztery=xv^4, xpiec=xv^5,xszesc=xv^6,xsiedem=xv^7,xosiem=xv^8,xdziewiec=xv^9,xdziesiec=xv^10)) 
+        lines(xv,yv,col="green")
+        }
     
     if(input$opcje == 'NO2')
     {
       y=c(NO2[x])
       plot(x,y)
+      fit=lm(y~x+xdwa+xtrzy+xcztery+xpiec+xszesc+xsiedem+xosiem+xdziewiec+xdziesiec)
+      xv=seq(min(x),max(x),0.01)
+      yv = predict(fit, list (x=xv, xdwa=xv^2, xtrzy=xv^3, xcztery=xv^4, xpiec=xv^5,xszesc=xv^6,xsiedem=xv^7,xosiem=xv^8,xdziewiec=xv^9,xdziesiec=xv^10)) 
+      lines(xv,yv,col="red")
     }
     
     if(input$opcje == 'PM10')
     {
       y=c(PM10[x])
       plot(x,y)
+      fit=lm(y~x+xdwa+xtrzy+xcztery+xpiec+xszesc+xsiedem+xosiem+xdziewiec+xdziesiec)
+      xv=seq(min(x),max(x),0.01)
+      yv = predict(fit, list (x=xv, xdwa=xv^2, xtrzy=xv^3, xcztery=xv^4, xpiec=xv^5,xszesc=xv^6,xsiedem=xv^7,xosiem=xv^8,xdziewiec=xv^9,xdziesiec=xv^10)) 
+      lines(xv,yv,col="red")
     }
     
     if(input$opcje == 'PM25')
     {
       y=c(PM25[x])
       plot(x,y)
+      fit=lm(y~x+xdwa+xtrzy+xcztery+xpiec+xszesc+xsiedem+xosiem+xdziewiec+xdziesiec)
+      xv=seq(min(x),max(x),0.01)
+      yv = predict(fit, list (x=xv, xdwa=xv^2, xtrzy=xv^3, xcztery=xv^4, xpiec=xv^5,xszesc=xv^6,xsiedem=xv^7,xosiem=xv^8,xdziewiec=xv^9,xdziesiec=xv^10)) 
+      lines(xv,yv,col="blue")
     }
     
     if(input$opcje == 'SO2')
     {
       y=c(SO2[x])
       plot(x,y)
+      fit=lm(y~x+xdwa+xtrzy+xcztery+xpiec+xszesc+xsiedem+xosiem+xdziewiec+xdziesiec)
+      xv=seq(min(x),max(x),0.01)
+      yv = predict(fit, list (x=xv, xdwa=xv^2, xtrzy=xv^3, xcztery=xv^4, xpiec=xv^5,xszesc=xv^6,xsiedem=xv^7,xosiem=xv^8,xdziewiec=xv^9,xdziesiec=xv^10)) 
+      lines(xv,yv,col="orange")
     }
     
     if(input$opcje == 'C6H6')
   { 
       y=c(C6H6[x])
       plot(x,y)
+      fit=lm(y~x+xdwa+xtrzy+xcztery+xpiec+xszesc+xsiedem+xosiem+xdziewiec+xdziesiec)
+      xv=seq(min(x),max(x),0.01)
+      yv = predict(fit, list (x=xv, xdwa=xv^2, xtrzy=xv^3, xcztery=xv^4, xpiec=xv^5,xszesc=xv^6,xsiedem=xv^7,xosiem=xv^8,xdziewiec=xv^9,xdziesiec=xv^10)) 
+      lines(xv,yv,col="brown")
     }
     
   })
@@ -62,7 +95,9 @@ shinyServer(function(input, output) {
     if(is.null(input$plotClick)) return("Wskaz wartosc kursorem\n")
     paste0("Data= ",floor(input$plotClick$x), "\nWartosc=", round(input$plotClick$y,1))
     
-  })  
+  })
+  
+  
   
   
  # output$wykres <- renderPlot({
