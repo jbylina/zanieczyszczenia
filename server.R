0244d635a879f5bb8270acd9726b3e1a019b88a5
Jacek Bylina <jacek.bylina@gmail.com>
2016-12-26 22:49:29 +0100

    Reformat R files

--- server.R
+++ server.R
@@ -1,121 +1,134 @@
 shinyServer(function(input, output) {
-  
-  dane = read.table(file = "bazaa.csv", header = TRUE, sep =";")
-  
+  dane = read.table(file = "bazaa.csv", header = TRUE, sep = ";")
   
   attach(dane)
   
   data_odczytu = as.numeric(data_odczytu)
   output$wykres <- renderPlot({
-      
-    x=seq(input$Daty[1],input$Daty[2], by = 1)
-    xdwa=x^2
-    xtrzy=x^3
-    xcztery=x^4
-    xpiec=x^5
-    xszesc=x^6
-    xsiedem=x^7
-    xosiem = x^8
-    xdziewiec = x^9
-    xdziesiec = x^10
-     
-   
-     if(input$opcje == 'O3')
-     {
-        y=c(O3[x])
-        plot(x,y) 
-        fit=lm(y~x+xdwa+xtrzy+xcztery+xpiec+xszesc+xsiedem+xosiem+xdziewiec+xdziesiec)
-        xv=seq(min(x),max(x),0.01)
-        yv = predict(fit, list (x=xv, xdwa=xv^2, xtrzy=xv^3, xcztery=xv^4, xpiec=xv^5,xszesc=xv^6,xsiedem=xv^7,xosiem=xv^8,xdziewiec=xv^9,xdziesiec=xv^10)) 
-        lines(xv,yv,col="green")
-        }
+    x = seq(input$Daty[1],input$Daty[2], by = 1)
+    xdwa = x ^ 2
+    xtrzy = x ^ 3
+    xcztery = x ^ 4
     
-    if(input$opcje == 'NO2')
+    xpiec = x ^ 5
+    xszesc = x ^ 6
+    xsiedem = x ^ 7
+    xosiem = x ^ 8
+    xdziewiec = x ^ 9
+    xdziesiec = x ^ 10
+    
+    
+    if (input$opcje == 'O3')
     {
-      y=c(NO2[x])
+      y = c(O3[x])
       plot(x,y)
-      fit=lm(y~x+xdwa+xtrzy+xcztery+xpiec+xszesc+xsiedem+xosiem+xdziewiec+xdziesiec)
-      xv=seq(min(x),max(x),0.01)
-      yv = predict(fit, list (x=xv, xdwa=xv^2, xtrzy=xv^3, xcztery=xv^4, xpiec=xv^5,xszesc=xv^6,xsiedem=xv^7,xosiem=xv^8,xdziewiec=xv^9,xdziesiec=xv^10)) 
-      lines(xv,yv,col="red")
+      fit = lm(y ~ x + xdwa + xtrzy + xcztery + xpiec + xszesc + xsiedem +
+                 xosiem + xdziewiec + xdziesiec)
+      xv = seq(min(x),max(x),0.01)
+      yv = predict(
+        fit, list (
+          x = xv, xdwa = xv ^ 2, xtrzy = xv ^ 3, xcztery = xv ^ 4, xpiec = xv ^ 5,xszesc =
+            xv ^ 6,xsiedem = xv ^ 7,xosiem = xv ^ 8,xdziewiec = xv ^ 9,xdziesiec = xv ^
+            10
+        )
+      )
+      lines(xv,yv,col = "green")
     }
     
-    if(input$opcje == 'PM10')
+    if (input$opcje == 'NO2')
     {
-      y=c(PM10[x])
+      y = c(NO2[x])
       plot(x,y)
-      fit=lm(y~x+xdwa+xtrzy+xcztery+xpiec+xszesc+xsiedem+xosiem+xdziewiec+xdziesiec)
-      xv=seq(min(x),max(x),0.01)
-      yv = predict(fit, list (x=xv, xdwa=xv^2, xtrzy=xv^3, xcztery=xv^4, xpiec=xv^5,xszesc=xv^6,xsiedem=xv^7,xosiem=xv^8,xdziewiec=xv^9,xdziesiec=xv^10)) 
-      lines(xv,yv,col="red")
+      fit = lm(y ~ x + xdwa + xtrzy + xcztery + xpiec + xszesc + xsiedem +
+                 xosiem + xdziewiec + xdziesiec)
+      xv = seq(min(x),max(x),0.01)
+      yv = predict(
+        fit, list (
+          x = xv, xdwa = xv ^ 2, xtrzy = xv ^ 3, xcztery = xv ^ 4, xpiec = xv ^ 5,xszesc =
+            xv ^ 6,xsiedem = xv ^ 7,xosiem = xv ^ 8,xdziewiec = xv ^ 9,xdziesiec = xv ^
+            10
+        )
+      )
+      lines(xv,yv,col = "red")
     }
     
-    if(input$opcje == 'PM25')
+    if (input$opcje == 'PM10')
     {
-      y=c(PM25[x])
+      y = c(PM10[x])
       plot(x,y)
-      fit=lm(y~x+xdwa+xtrzy+xcztery+xpiec+xszesc+xsiedem+xosiem+xdziewiec+xdziesiec)
-      xv=seq(min(x),max(x),0.01)
-      yv = predict(fit, list (x=xv, xdwa=xv^2, xtrzy=xv^3, xcztery=xv^4, xpiec=xv^5,xszesc=xv^6,xsiedem=xv^7,xosiem=xv^8,xdziewiec=xv^9,xdziesiec=xv^10)) 
-      lines(xv,yv,col="blue")
+      fit = lm(y ~ x + xdwa + xtrzy + xcztery + xpiec + xszesc + xsiedem +
+                 xosiem + xdziewiec + xdziesiec)
+      xv = seq(min(x),max(x),0.01)
+      yv = predict(
+        fit, list (
+          x = xv, xdwa = xv ^ 2, xtrzy = xv ^ 3, xcztery = xv ^ 4, xpiec = xv ^ 5,xszesc =
+            xv ^ 6,xsiedem = xv ^ 7,xosiem = xv ^ 8,xdziewiec = xv ^ 9,xdziesiec = xv ^
+            10
+        )
+      )
+      lines(xv,yv,col = "red")
     }
     
-    if(input$opcje == 'SO2')
+    if (input$opcje == 'PM25')
     {
-      y=c(SO2[x])
+      y = c(PM25[x])
       plot(x,y)
-      fit=lm(y~x+xdwa+xtrzy+xcztery+xpiec+xszesc+xsiedem+xosiem+xdziewiec+xdziesiec)
-      xv=seq(min(x),max(x),0.01)
-      yv = predict(fit, list (x=xv, xdwa=xv^2, xtrzy=xv^3, xcztery=xv^4, xpiec=xv^5,xszesc=xv^6,xsiedem=xv^7,xosiem=xv^8,xdziewiec=xv^9,xdziesiec=xv^10)) 
-      lines(xv,yv,col="orange")
+      fit = lm(y ~ x + xdwa + xtrzy + xcztery + xpiec + xszesc + xsiedem +
+                 xosiem + xdziewiec + xdziesiec)
+      xv = seq(min(x),max(x),0.01)
+      yv = predict(
+        fit, list (
+          x = xv, xdwa = xv ^ 2, xtrzy = xv ^ 3, xcztery = xv ^ 4, xpiec = xv ^ 5,xszesc =
+            xv ^ 6,xsiedem = xv ^ 7,xosiem = xv ^ 8,xdziewiec = xv ^ 9,xdziesiec = xv ^
+            10
+        )
+      )
+      lines(xv,yv,col = "blue")
     }
     
-    if(input$opcje == 'C6H6')
-  { 
-      y=c(C6H6[x])
+    if (input$opcje == 'SO2')
+    {
+      y = c(SO2[x])
       plot(x,y)
-      fit=lm(y~x+xdwa+xtrzy+xcztery+xpiec+xszesc+xsiedem+xosiem+xdziewiec+xdziesiec)
-      xv=seq(min(x),max(x),0.01)
-      yv = predict(fit, list (x=xv, xdwa=xv^2, xtrzy=xv^3, xcztery=xv^4, xpiec=xv^5,xszesc=xv^6,xsiedem=xv^7,xosiem=xv^8,xdziewiec=xv^9,xdziesiec=xv^10)) 
-      lines(xv,yv,col="brown")
+      fit = lm(y ~ x + xdwa + xtrzy + xcztery + xpiec + xszesc + xsiedem +
+                 xosiem + xdziewiec + xdziesiec)
+      xv = seq(min(x),max(x),0.01)
+      yv = predict(
+        fit, list (
+          x = xv, xdwa = xv ^ 2, xtrzy = xv ^ 3, xcztery = xv ^ 4, xpiec = xv ^ 5,xszesc =
+            xv ^ 6,xsiedem = xv ^ 7,xosiem = xv ^ 8,xdziewiec = xv ^ 9,xdziesiec = xv ^
+            10
+        )
+      )
+      lines(xv,yv,col = "orange")
     }
     
+    if (input$opcje == 'C6H6')
+    {
+      y = c(C6H6[x])
+      plot(x,y)
+      fit = lm(y ~ x + xdwa + xtrzy + xcztery + xpiec + xszesc + xsiedem +
+                 xosiem + xdziewiec + xdziesiec)
+      xv = seq(min(x),max(x),0.01)
+      yv = predict(
+        fit, list (
+          x = xv, xdwa = xv ^ 2, xtrzy = xv ^ 3, xcztery = xv ^ 4, xpiec = xv ^ 5,xszesc =
+            xv ^ 6,xsiedem = xv ^ 7,xosiem = xv ^ 8,xdziewiec = xv ^ 9,xdziesiec = xv ^
+            10
+        )
+      )
+      lines(xv,yv,col = "brown")
+    }
   })
   
-  output$text1 <- renderText({ 
-          
-   (paste("Wykres zaleznosci", input$opcje, "od czasu"))
+  output$text1 <- renderText({
+    (paste("Wykres zaleznosci", input$opcje, "od czasu"))
     
-    })
+  })
 
-  #paste0("x=", round(e$x, 1), " y=", round(e$y, 1), "\n")
-    
   output$info <- renderText({
-    
-    if(is.null(input$plotClick)) return("Wskaz wartosc kursorem\n")
+    if (is.null(input$plotClick))
+      return("Wskaz wartosc kursorem\n")
     paste0("Data= ",floor(input$plotClick$x), "\nWartosc=", round(input$plotClick$y,1))
-    
   })
-  
-  
-  
-  
- # output$wykres <- renderPlot({
-    
- #   x=seq(input$Daty[1],input$Daty[2], by = 1)
-    
-  #  if(input$opcje == 'O3')
-  #  {
-   #   y=c(O3[x])
-      
-    #  approxfun(x, y)
-      
-    #}
-    
-    
-    
-    
-  #})
-  
-  
 })
--- ui.R
+++ ui.R
@@ -1,42 +1,39 @@
-
-  shinyUI(fluidPage(
-  titlePanel("Zanieczyszczenia powietrza"),
-  br(),
-  style = 'background-image:url("tlo.png")',
-  sidebarLayout(
-    sidebarPanel( helpText("Nasza aplikacja sluzy przedstawieniu poziomu zanieczyszczen powietrza z ostatnich X dni oraz przedstawieniu przewidywanego poziomu zanieczyszczen na nastepne Y dni"),
-    selectInput("opcje",
-                label="Wybierz interesujace Cie informacje",
-                choices=c("PM10"="PM10", "PM25", "O3"="O3", "NO2","SO2","C6H6"),
-                selected = "O3"),
-    
-    sliderInput("Daty",label="Podaj interesujacy Cie przedzial czasu",
-                   min = 1,
-                   max = 287,
-                   value=c(69,72) 
-                    )
-                  # start = Sys.Date() - 10, 
-                  # end = Sys.Date() + 3,
-                  # min = Sys.Date() - 10,
-                  # max = Sys.Date() + 3 )
-  ),
-  
- 
-  
-  mainPanel(h3(textOutput("text1")),
-            plotOutput("wykres", click = "plotClick"),
-            verbatimTextOutput("info")
-            
-            
-            
-            )
-           
-            
-            )
-           
-            
-          
-            
-)
-
+shinyUI(
+  fluidPage(
+    titlePanel("Zanieczyszczenia powietrza"),
+    br(),
+    style = 'background-image:url("tlo.png")',
+    sidebarLayout(
+      sidebarPanel(
+        helpText(
+          "Nasza aplikacja sluzy przedstawieniu poziomu zanieczyszczen powietrza z ostatnich X dni oraz przedstawieniu przewidywanego poziomu zanieczyszczen na nastepne Y dni"
+        ),
+        selectInput(
+          "opcje",
+          label = "Wybierz interesujace Cie informacje",
+          choices = c(
+            "PM10" = "PM10", "PM25", "O3" = "O3", "NO2","SO2","C6H6"
+          ),
+          selected = "O3"
+        ),
+        
+        sliderInput(
+          "Daty",label = "Podaj interesujacy Cie przedzial czasu",
+          min = 1,
+          max = 287,
+          value = c(69,72)
+        )
+        # start = Sys.Date() - 10,
+        # end = Sys.Date() + 3,
+        # min = Sys.Date() - 10,
+        # max = Sys.Date() + 3 )
+      ),
+      
+      mainPanel(
+        h3(textOutput("text1")),
+        plotOutput("wykres", click = "plotClick"),
+        verbatimTextOutput("info")
+      )
+    )
+  )
 )
